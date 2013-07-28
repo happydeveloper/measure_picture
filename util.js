6c1312f6c6cc89a464ec07434ff27a2564d5d1b8
DU RU KANG <enfn2001@gmail.com>
2013-07-28 23:32:05 +0900

    코드 구조화 작�
    코드 구조화 작업 - TDD 적용 가능하도록 변경

--- util.js
+++ util.js
@@ -1,7 +1,14 @@
 
+	//객체지향적으로 변환 필요
+	
+
 	var flag = 0;
 	// 클릭한 이후 두 점을 가져오는 함수
 	var cr = [];
+	
+
+	//플래그를 셋팅한다.
+	//칼리브레이션을 하고 있는 상태를 표시한다.
 	function get_two_point() {
 		console.log('entry funcion');
 		switch(flag) 
@@ -32,6 +39,8 @@
 			
 			pos_x = x-event.currentTarget.offsetLeft;
 			pos_y = y-event.currentTarget.offsetTop;
+			
+//로직의 분리 필요
 			var result = document.getElementById('result');
 
 			if(flag == 1){
@@ -119,9 +128,43 @@
 		context.stroke();
 	}
 
+	//거리 계산 로직
 	function distance_cal(x1, y1, x2, y2)
 	{
 		var result = Math.pow((x2-x1),2) + Math.pow((y2-y1),2);
 		result = Math.sqrt(result); 	
 		console.log("distence" + result);
 	}
+
+	//2560, 1920
+	function Cal_ratio_img_win(actual_img_pixel_width, win_img_pixel_width)
+	{
+		return ratio_img_win = actual_img_pixel_width / win_img_pixel_width;
+	}
+	function wpxToipx(windows_pixcel_measure_distance, ratio_img_win){
+		//actual image width pixel/canvas image width pixel
+		result = this.ratio_img_win * windows_pixcel_measure_distance;
+		document.getElementById("result_disply").innerHTML = "<p> 실제 사진 픽셀 거리는 :: "+ result + " 이다.</p>";
+	}
+     
+
+     //실제 크기와 현재 이미지간에 비율을 구할 수 있다.
+     //parameter 1 : 실제 측정 기준(자) 거리 
+     //parameter 2 : 원도우 측정된 기준자 픽셀 거리
+     //return : 실제 거리와 이미지간에 비율을 리턴
+     function getRatio(actual_ruler_distance_val, windows_ruler_fixel_distance_val) {
+		return ratio_actRul_winRul=actual_ruler_distance_val/windows_ruler_fixel_distance_val;
+	}
+
+	//실제 거리 구하기 = 원도우 필셀을 실제 거리로 환산
+	//parameter 1 : 원도우 이미지와 실제 이미지이의 비율 
+	//parameter 2 : 원도우에서 측정한 거리
+	//return : 실제 거리
+	function getWpxToApx(ratio_actRul_winRul, winPix ){
+		return ratio_actRul_winRul * winPix;
+	}
+	
+
+
+
+
